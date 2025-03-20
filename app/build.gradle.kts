e96600bf68b0daa1bff06ae10dd867a43e040870
Saif Raditha <saifapriansyah596@gmail.com>
2025-03-20 10:31:54 +0700

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.smkth.app_finalproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.smkth.app_finalproject"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+    viewBinding {
+        enable= true
     }
 }
 
